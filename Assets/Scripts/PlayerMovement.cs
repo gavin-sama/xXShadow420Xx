3d9c376df74683173b0fb3483948c75952b4a352
Landon S. <landonstott1@gmail.com>
2025-05-03 21:02:59 -0400

    Changed a poorly used method.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -140,9 +140,11 @@ public class PlayerMovement : MonoBehaviour
             playerCamera.transform.localRotation = Quaternion.Euler(rotationY, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
+
+        AnimationHandler();
     }
 
-    private void FixedUpdate()
+    private void AnimationHandler()
     {
         // Handle animations - only if not currently jumping
         if (!isJumpingAnimation)

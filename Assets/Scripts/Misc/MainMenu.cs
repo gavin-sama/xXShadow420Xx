cefb113d473e803f865b9b80599f7d75a82626e7
Landon S. <landonstott1@gmail.com>
2025-05-04 21:21:41 -0400

    Set up a basic main menu template without functionality.

---
+++ Assets/Scripts/Misc/MainMenu.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MainMenu : MonoBehaviour
+{    
+    public GameObject newButton;
+    public GameObject loadButton;
+
+    private int gameSaves;
+
+    private void Start()
+    {
+        gameSaves = SavesMenu.Saves;
+
+        if (gameSaves == 4)
+            newButton.GetComponent<Button>().interactable = false;
+
+        if (gameSaves == 0)
+            loadButton.GetComponent<Button>().interactable = false;
+    }
+
+    public void NewGame()
+    {
+        // Open the canvas for the 4 load saves
+        // Overwrite the option chosen
+        // Clone and launch the fresh start scene
+    }
+
+    public void LoadGame()
+    {
+        // Open the canvas for the 4 load saves
+        // Launch the option chosen
+
+        SavesMenu.savesCanvas.SetActive(true);
+    }
+
+    public void Options()
+    {
+        // Open the settings canvas
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Misc/SavesMenu.cs
@@ -0,0 +1,18 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SavesMenu : MonoBehaviour
+{
+    public static int Saves { get; private set; }
+    public static GameObject savesCanvas;
+
+    public void CloneNewSave()
+    {
+        // Create a new save from the base scene with load
+    }
+
+    public void LoadSave()
+    {
+        // Load one of the 4 saves if 
+    }
+}

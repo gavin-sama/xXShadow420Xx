c55a59783ea45428aefe9d54ccdf76d68d82d61d
Gavin Walter <walterg@etsu.edu>
2025-08-13 22:34:13 -0500

    Revert "Merge pull request #84 from gavin-sama/landon"
    This reverts commit 074d638bd63c7a4f76dfb1cd21d477ad0b5ea116, reversing
    changes made to 43574c3aaaf0572e54be9f12ee18c95b403d17f4.

--- Assets/Scripts/Map/Ground/Tile.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Tile
-{
-    public Tile[] northNeighbours;
-    public Tile[] eastNeighbours;
-    public Tile[] southNeighbours;
-    public Tile[] westNeighbours;
-}
